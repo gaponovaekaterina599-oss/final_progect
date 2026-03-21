58fa741381382190b788daf1d7d19ec731fe0ba6
Pro <mvke_money@mail.ru>
2026-03-21 20:24:02 +0300

    tz for Kaya

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-print("hello word")
-print("ki")
-print("gigigrik")
+from flask import Flask
+from flask import render_template
 
-print("hi Pavil and hi Elizoveta")
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
