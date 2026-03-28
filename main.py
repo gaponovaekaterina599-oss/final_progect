05e84393346313059a565edd7b5cc7736ba301d2
Detskiy <gaponovekaterina599@gmail.com>
2026-03-28 20:30:41 +0300

    for Pavil

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from flask import render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST', ])
 def index():
     if request.method == "GET":
         return render_template("index.html")
     else:
+        print('post')
         print(request.form.get('user_input'))
-
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
