f7fb1132b7344fcf532e3882360b5842b5854fc1
Detskiy <gaponovekaterina599@gmail.com>
2026-04-04 21:16:29 +0300

    for Pavil

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ def index():
     if request.method == "GET":
         return render_template("index.html")
     else:
+
         print('post')
         print(user.append(request.form.get('user_input')))
+        print(user)
         return render_template("index.html")
 
-
+@app.route('/to_do', methods=['GET', 'POST', ])
+def to_do():
+    if request.method == "GET":
+        return render_template("2.html",user=user)
 
 if __name__ == "__main__":
     app.run()
