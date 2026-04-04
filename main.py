74e9da2ed4c1149284962a084a235cb5565556fd
Detskiy <gaponovekaterina599@gmail.com>
2026-04-04 20:09:29 +0300

    for Pavil

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ from flask import render_template, request
 app = Flask(__name__)
 user = []
 
-
-
-
-
 @app.route('/', methods=['GET', 'POST', ])
 def index():
     if request.method == "GET":
         return render_template("index.html")
     else:
         print('post')
-        user.append(request.form.get('user_input'))
-        print(user)
+        print(user.append(request.form.get('user_input')))
         return render_template("index.html")
+
+
 
 if __name__ == "__main__":
     app.run()
+
+
+# Todo 1 сделать страницу html css с задачами (Пример: 1 - задача 1
+#                                                      2 - задача 2)
+
+# Todo 2 user передавать на стрницу html с помощью for выводить данные (урок 20)
